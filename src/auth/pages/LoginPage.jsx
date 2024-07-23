d6473ccd339c3c9e66b9b1d339ff74fb770f49a2
Boris Sea <seaborisrc@gmail.com>
2024-07-23 12:24:17 -0400

    TD-10 - Configuration Redux Toolkit

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,11 +2,28 @@ import { Link as RouterLink } from "react-router-dom";
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
 
 export const LoginPage = () => {
+
+  const { email, password, onInputChange, formState } = useForm({
+    email: 'chester@google.com',
+    password: '123456789'
+  });
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+
+    console.log({ email, password });
+  }
+
+  const onGoogleSignIn = () => {
+    console.log('google...')
+  }
+
   return (
     <AuthLayout title='Login'>
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +31,9 @@ export const LoginPage = () => {
               type="email"
               placeholder="email@example.com"
               fullWidth
+              name="email"
+              value={ email }
+              onChange={ onInputChange }
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,16 +42,19 @@ export const LoginPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={ password }
+              onChange={ onInputChange }
             />
           </Grid>
           <Grid container spacing={2} sx={{ mb: 1, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Login
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button onClick={ onGoogleSignIn } variant="contained" fullWidth>
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -2,7 +2,7 @@ import { ImageList, ImageListItem }  from '@mui/material';
 
 export const ImageGallery = () => {
   return (
-    <ImageList sx={{ width: "100%", height: 500 }} cols={4} rowHeight={100}>
+    <ImageList sx={{ width: "100%", height: 900 }} cols={4} rowHeight={100}>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <img
