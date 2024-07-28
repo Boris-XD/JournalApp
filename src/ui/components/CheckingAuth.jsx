ca34331c08c44c3b29b69ede5f14cd08190c9cb4
Boris Sea <seaborisrc@gmail.com>
2024-07-27 21:57:47 -0400

    Authentication - update routes when validate with FireBase

--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -13,7 +13,7 @@ const CheckingAuth = () => {
       sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
     >
       <Grid
-        item
+        container
         direction='row'
         justifyContent='center'
       >
